422ba9ae7957a3189dc16f751750cf09cc134714
Proiky <mujkan.98@gmail.com>
2022-08-13 00:19:36 +0200

    Variables assignment

---
+++ section2/Assignments/assignments.js
@@ -0,0 +1,9 @@
+alert("Welcome to my country!")
+
+let myCountry = "Bosnia and Herzegovina";
+let myContinent = "Europe";
+let populationOfMyCountry = 3300000;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(populationOfMyCountry);
