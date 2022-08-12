5e1423f7e7f85732b147a3f063da6ea63ccf0d11
Proiky <mujkan.98@gmail.com>
2022-08-12 21:22:02 +0200

    changes

---
+++ section2/2definitionOfJS.js
@@ -0,0 +1,22 @@
+/* What is JS?
+
+JS instructs computer to do something! We dont have think about managing computer memory which is
+ what makes him a high level programming language.
+ It is Object oriented! 
+ On web we use JSS, HTML and JS to make applications. HTML is responsible for content on web-page
+ CSS is responsible for presentation where JS is a programming language of the internet.
+ We use it for building web app's.
+ JS allows us to add dynamic effects and make whole applications in the browser!
+ ANGULAR, REACT - tools that make writing large-scale apps faster and easier! but they are still
+ 100% based on JS.
+
+ FRONT-END:
+ when we write JS apps in a browser
+
+ BACK-END:
+ (nodejs) -> when we use JS outside of the browser enviroment, communicationing on web servers
+
+ ES6 or ES2015 -> biggest update to the language EVER.
+ from 2015 its considered to be MODERN JAVA SCRIPT! -> which we are going to learn and use.
+ */
+
