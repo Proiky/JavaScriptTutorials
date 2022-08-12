d1afa74cbe8cf4139e8d0a3a4f80eef7337e6703
Proiky <mujkan.98@gmail.com>
2022-08-12 21:53:45 +0200

    Linking-JS-with-html

---
+++ section2/3Linking/script.js
@@ -0,0 +1,17 @@
+let js = "amazing";
+if (js === "amazing") alert("JS is fun!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10) // sad ga vidimo u desni klik -> inspect -> console!
+
+/*Kod iz index.html smo prebacili ovdje kako bi tamosnji kod bio cisci i ljepsi
+iako smo to uradili index.html ne zna gdje se ovo nalazi i moramo ih povezati!
+to radimo na sl nacin:
+to radimo na kraju body-taga i koristimo opet kljunu rijec script. Ne ubacujemo nikakav kod neko samo specificiramo source!
+na nacin:
+<script src = "script.js"></script>
+
+naravno script.js i index.html moraju bit u istom fajlu da bi ga on pravilno prepoznao!
+kad smo ovo dvoje odvojili, vise nam i ne treba index.html, sve promjene vrsimo ovdje!
+
+*/
