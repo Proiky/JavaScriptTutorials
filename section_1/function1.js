a7b5486391b0e7db837ccd85664b54a141098e15
Proiky <mujkan.98@gmail.com>
2022-08-12 21:02:35 +0200

    dodavanje funkcije oduzima

--- section_1/function1.js
+++ section_1/function1.js
@@ -1,11 +1,15 @@
-function sabira(){
-    return a+b;
+function sabira() {
+    return a + b;
 }
 
-function oduzima(){
-    return a-b;
+function oduzima() {
+    return a - b;
 }
 
-function mnozi(){
-    return a*b;
+function mnozi() {
+    return a * b;
+}
+
+function oduzima() {
+    return a - b;
 }
