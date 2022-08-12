3a8adfdf1b88b3f1d5f84b1e03d48f758ed35750
Proiky <mujkan.98@gmail.com>
2022-08-13 00:10:51 +0200

    variables and values

--- section2/4Variables/script.js
+++ section2/4Variables/script.js
@@ -1,2 +1,42 @@
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+//--------------------------------------------------------
+//Koristit cemo osobu kao primjer u nastavku:
+//Value -> piece of data and the most fundamental unit in programming
+
+console.log('Jonas');
+console.log(23); //ovo su sve vrijedosti!
+//--------------------------------------------------------
+
+// Naravno spremimo ih u varijable i mozemo ih koristiti konstatno kroz nas kod! npr:
+let firstName = "Sivko"; //let -> jedan od nacina da se kreira varijabla!
+console.log(firstName);
+//-------------------------------------------------------
+
+/* Conventions and rules
+
+1. Cammel case -> ako imamo vise rijeci unutar nase varijable pocnemo malim slovom, a sve ostale rijeci pocnemo velikim slovom kao npr:
+firstName.
+2. We cant use spaces in our variable names, or start with a number!
+3. Variable names can only contain characters, numbers, underscore or dollar sign!
+4. We cant name our variable as reserved JS keyword! npr: new
+5. We CAN start variables with _
+6. word "name" is sometimes reserved and should be avoided!
+7. Convention: We shouldnt start variable name with UPPER CASE LETTER!
+8. Convention: If we want to declare and initalize constants we should write the whole variable name UPPER CASE! npr: let PI = 3.1415; mada obicno koristimo const!
+9. Convention: Our variable names should be descriptive and that is very important!
+*/
+
+// convention 9:
+
+let myFirstJob = "programmer";
+let mySecondJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "techer";
+
+console.log(myFirstJob);
+//We can see that the first approach is much better cause we can contain much more info just based on variable name!
+
+
+
