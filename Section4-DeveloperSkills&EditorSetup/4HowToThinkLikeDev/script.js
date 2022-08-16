751a26589d10010684d041cd1741c98c46871ab2
Proiky <mujkan.98@gmail.com>
2022-08-16 21:35:39 +0200

    How to think like a dev and solve problems

---
+++ Section4-DeveloperSkills&EditorSetup/4HowToThinkLikeDev/script.js
@@ -0,0 +1,17 @@
+// HOW TO THINK LIKE DEV, BECOME A PROBLEM SOLVER!
+
+/*
+4 STEPS TO SOLVE ANY PROBLEM;
+
+1. Make sure you 100% understand the problem!! Always make sure to ask a right question, after you understand the problem.
+2. DIVIDE & CONQUER -> break big problems into smaller sub-problems!
+3. Do as much research as you possibly can! Internet is your friend
+4. For bigger problems write pseudo-code, before writing the actual code!
+*/
+
+//PSEUDO-CODE is nebulozan kod koji je lako razumljiv, ne mora uopste biti postojeci jezik
+//bitno je pomocu necega sto vise pojednostaviti problem kako bi se mogao lakse predstaviti pomocu
+//programskog jezika!!
+
+//+ NAJVAZNIJI KORAK
+//STVORITI VELIKU ZAINTERESOVANOST ZA TO STO RADIMO. Tako nam posao nije tezak i konstantno se razvijamo!
