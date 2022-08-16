673ab6421d045cbdbb68f8bb0e5e2f574007f181
Proiky <mujkan.98@gmail.com>
2022-08-16 13:19:42 +0200

    For petlja

---
+++ Section3-Fundamentals/11ForLoop/script.js
@@ -0,0 +1,23 @@
+// PETLJE - for petlja
+
+//sintaksa je ista kao u c++-u!
+
+/*
+console.log(`Lifting weights repetiotion 1`);
+console.log(`Lifting weights repetiotion 2`);
+console.log(`Lifting weights repetiotion 3`);
+console.log(`Lifting weights repetiotion 4`);
+console.log(`Lifting weights repetiotion 5`);
+console.log(`Lifting weights repetiotion 6`);
+console.log(`Lifting weights repetiotion 7`);
+console.log(`Lifting weights repetiotion 8`);
+console.log(`Lifting weights repetiotion 9`);
+*/
+
+//ovo mozemo sve zamijeniti for petljom!
+
+//for loop keeps running while condition i<=10 is true! As soon as it is false it stops!
+for (let i = 0; i <= 10; i++) {
+    console.log(`Lifting weights repetition ${i}`);
+}
+//to je sintaksa for petlje, nema puno teorije oko ovoga, sve je jako intuitivno 
