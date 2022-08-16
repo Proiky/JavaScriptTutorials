860a28437351a584c08f023fab80d0894512d147
Proiky <mujkan.98@gmail.com>
2022-08-16 21:23:38 +0200

    Rules you should follow to become efficient dev

---
+++ Section4-DeveloperSkills&EditorSetup/3LearningHowToCode/script.js
@@ -0,0 +1,14 @@
+// RULES FOR BECOMING EFFICIENT DEVELOPER
+
+/*
+1. Set a clear realistic goal!
+2. NEVER copy a code without understanding!
+3. REINFORCING, -> after learning its really important to use it really quick to make it stick in the brain (notes + reviews + coding exercises + challenges);
+4. codewars.com -> veoma dobra stranica za challenges!
+5. Always practice coding on your own, and come up with your own ideas
+6. Dont get frustrated and take it easy, think about your code!
+7. Accept that you will NEVER know everything!
+8. Learn together with other people and explain concepts to other people!
+9. 
+
+*/
