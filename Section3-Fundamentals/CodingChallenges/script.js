59108eef56f6f3efe297d53ac9736888dad1b202
Proiky <mujkan.98@gmail.com>
2022-08-16 14:59:20 +0200

    coding challenge 4-part2

--- Section3-Fundamentals/CodingChallenges/script.js
+++ Section3-Fundamentals/CodingChallenges/script.js
@@ -149,6 +149,7 @@ respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 tall. */
 
+/*
 const mark = {
     firstName: 'Mark',
     lastName: 'Miller',
@@ -196,3 +197,78 @@ if (mark.BMI > john.BMI) {
 } else {
     console.log(`They have the same BMI`);
 }
+*/
+
+
+//--------------------CODING CHALLENGE 4--------------------------//
+
+/*Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before(no need to repeat) to calculate
+tips and total values(bill + tip) for every bill value in the bills array.Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+    an argument.This function calculates the average of all numbers in the given
+array.This is a difficult challenge(we haven't done this before)! Here is how to
+solve it:
+    4.1.
+        First, you will need to add up all values in the array.To do the addition,
+            start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop.In each iteration, add the current value to the
+'sum' variable.This way, by the end of the loop, you have all values
+added together
+4.2.
+To calculate the average, divide the sum you calculated before by the
+length of the array(because that's the number of elements)
+4.3.
+Call the function with the 'totals' array
+*/
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+function caclTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+
+//for (let i = 0; i < bills.length; i++) {
+//   tips[i] = caclTip(bills[i]);
+//  totals[i] = tips[i] + bills[i];
+//}
+//treba da se radi sa push iako i ovo radi :)
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(caclTip(bills[i]));
+    totals.push(tips[i] + bills[i]);
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+
+//----BONUS----:
+
+function calcAverage(arr) {
+    let average = 0;
+    for (let i = 0; i < arr.length; i++) {
+        average += arr[i];
+    }
+    return average /= arr.length;
+}
+
+const test = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+
+console.log(calcAverage(totals));
+console.log(calcAverage(test));
