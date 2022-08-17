217881021a776e1b43e0f45f9979e603ecfd4444
Proiky <mujkan.98@gmail.com>
2022-08-17 10:07:40 +0200

    Intro do debugging

---
+++ Section4-DeveloperSkills&EditorSetup/6Debugging/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//extremely important developer skill!
+
+//Software bug -> defet or problem in coputer problam. ANy unexpected or unintended behavior of a computer program is a software bug.
+//previous exapmle: we need a function that reverses anything we pass into it
+
+//reverse ([1,3,5,7]); dobijemo npr [5,7,1,3];
+
+//1. IDENTIFY-ing bugs!
+//->mogu se identificirati u mnogo slucjeva - during development, testing software, user reports a bug, special bug in special browser itd.
+
+//2. FIND a bug
+//-> Ako je kod jednostavan lako ga je locirati
+//-> Ako kod nije jednostavan moramo koristiti debugger
+
+//3. FIX a bug
+//->replace wrong solution with a right solution
+
+//4. PREVENT this bug from happening again
+//->Searching for the same bug in similar code
+//->writing tests using testing software - very important
