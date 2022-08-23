ddd07adb12cd057c838d4ce3ff12559dce38ca47
Proiky <mujkan.98@gmail.com>
2022-08-23 14:49:06 +0200

    Reseting a game!

---
+++ Section6-DOMFundamentals/16ResetingGame/script.js
@@ -0,0 +1,95 @@
+"use strict";
+
+//-----------GAME FUNCTIONALITY---------//
+//----RESETING THE GAME-----//
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores;
+//const ne moze biti samo deklarisano nego mora biti i inicijalizirano
+let currentScore;
+let activePlayer;
+let playing;
+
+function pocetneVrijednosti() {
+  //ova funkcija treba da se pozove na pocetku koda, ili ako stisnemo button NEW GAME
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player--active");
+  player0El.classList.add("player--active");
+}
+
+pocetneVrijednosti();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //console.log(dice);
+
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+  }
+
+  if (scores[activePlayer] >= 10) {
+    playing = false;
+    diceEl.classList.add("hidden");
+
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+  } else {
+    switchPlayer();
+  }
+});
+
+btnNew.addEventListener("click", pocetneVrijednosti);
